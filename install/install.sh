b154c20c10f1228f25d81bb2df54cafc7b00b080
Corben Leo <corben@lynxsecurity.io>
2018-11-07 10:22:23 -0600

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -8,3 +8,6 @@ sudo apt-get install postfix -y
 sudo echo 'header_checks = regexp:/etc/postfix/ctf_flag' >> /etc/postfix/main.cf
 sudo echo '/^Subject:(.*)/ REPLACE Subject: flag{oMg_suCh_h4x_sSrf}' > /etc/postfix/ctf_flag
 service postfix reload
+ufw allow 22
+ufw allow 80
+ufw allow from 127.0.0.1 to 127.0.0.1 port 25 proto tcp
